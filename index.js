1d849d6cebda8820b65f5e9a93827db6be96106e
fayfang <fayfang@fayfangdeMacBook-Air.local>
2019-01-08 17:53:18 +0800

    加点备注吧，暂时想不到能有啥优化的

--- index.js
+++ index.js
@@ -7,9 +7,12 @@ const schema = {
 
 module.exports = function(source) {
   let options = loaderUtils.getOptions(this)
+  // validate
   validateOptions(schema, options, 'styleConfigLoader')
+  // getpath
   let configPath = options.configPath
   let pathString = loaderUtils.stringifyRequest(this, configPath)
+  // output
   let outString = `@import ${pathString};\n${source}`
 
   return outString
